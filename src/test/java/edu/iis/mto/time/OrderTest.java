812c4f0a49ff0493ce8e43fa0be54e04d57c75f0
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-04-18 15:27:47 +0200

    add test

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -4,16 +4,76 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
-
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
-
+    @Mock
+    private Clock clockMock;
+    private Order order;
+    private final Instant Date = Instant.parse("2005-01-01T10:00:00Z");;
     @BeforeEach
-    void setUp() throws Exception {}
-
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    void setUp() {
+        when(clockMock.getZone()).thenReturn(ZoneId.systemDefault());
+        order = new Order(clockMock);
     }
 
+    @Test
+    void atTheSameTimeShouldReturnConfirmed() {
+
+        Instant expirationTime = Date.plus(0, ChronoUnit.HOURS);
+        when(clockMock.instant()).thenReturn(Date).thenReturn(expirationTime);
+
+        try {
+            order.submit();
+            order.confirm();
+            assertSame(Order.State.CONFIRMED, order.getOrderState());
+        } catch (OrderExpiredException ignored) {
+            fail("failed");
+        }
+    }
+    @Test
+    void after6845616HoursShouldReturnCanceled() {
+
+        Instant expirationTime = Date.plus(6845616, ChronoUnit.HOURS);
+        when(clockMock.instant()).thenReturn(Date).thenReturn(expirationTime);
+        try {
+            order.addItem(new OrderItem());
+            order.submit();
+            order.confirm();
+            fail("failed");
+        } catch (OrderExpiredException ignored) {
+
+        }
+        Order.State orderState = order.getOrderState();
+        assertEquals(orderState, Order.State.CANCELLED);
+    }
+
+    @Test
+    void afterHoursShouldReturnConfirmed() {
+
+        Instant expirationTime = Date.plus(1, ChronoUnit.HOURS);
+        when(clockMock.instant()).thenReturn(Date).thenReturn(expirationTime);
+        try {
+            order.addItem(new OrderItem());
+            order.submit();
+            order.confirm();
+
+        } catch (OrderExpiredException ignored) {
+            fail("failed");
+
+        }
+        Order.State orderState = order.getOrderState();
+        assertEquals(orderState, Order.State.CONFIRMED);
+    }
 }
